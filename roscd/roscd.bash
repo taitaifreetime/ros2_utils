d7ae6bff70cd963c3b8361709abd5c4af2e91342
taitaifreetime <taiki11250628@gmail.com>
2024-08-12 15:16:46 +0900

    you can designate multiple work spaces to be searched

--- roscd/roscd.bash
+++ roscd/roscd.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# add ros work space to be serached
-ws_path="/your_ws"
+# add ros work spaces to be serached
+ws_paths=("/ws1" "/ws2") 
 
 # add path to ros
 ros_path="/opt/ros/humble"
@@ -13,28 +13,43 @@ _roscd() {
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
     if [[ "$prev" == "roscd" ]]; then
-        local pkg_list=$(ls $ws_path/src | grep -v -e "install" -e "build" -e "log")
-        local root_pkg_list=$(ls $ros_path/share)
+        local pkg_list=""
+        for ws in "${ws_paths[@]}"; do
+            local ws_pkg_list=$(ls "$ws/src" 2>/dev/null | grep -v -e "install" -e "build" -e "log")
+            pkg_list="$pkg_list $ws_pkg_list"
+        done
+        local root_pkg_list=$(ls "$ros_path/share")
         COMPREPLY=( $(compgen -W "$pkg_list $root_pkg_list" -- "$cur") )
         return 0
     fi
 }
 roscd() {
     local cd_path="$CDPATH"
-    export CDPATH="$ws_path/src:$ros_path/share"
-    if [ $# -eq 0 ]; then
-        echo "Usage: roscd <package_name>"
-    else
-        local pkg_path="$ws_path/src/$1"
-        local root_pkg_path="$ros_path/share/$1"
-        if [ -e "$pkg_path" ]; then
-            cd "$pkg_path"
-        elif [ -e "$root_pkg_path" ]; then
-            cd "$root_pkg_path"
+    local found_pkg=false
+
+    for ws in "${ws_paths[@]}"; do
+        export CDPATH="$ws/src:$ros_path/share"
+        if [ $# -eq 0 ]; then
+            echo "Usage: roscd <package_name>"
         else
-            echo "No such package"
+            local pkg_path="$ws/src/$1"
+            local root_pkg_path="$ros_path/share/$1"
+            if [ -e "$pkg_path" ]; then
+                cd "$pkg_path"
+                found_pkg=true
+                break
+            elif [ -e "$root_pkg_path" ]; then
+                cd "$root_pkg_path"
+                found_pkg=true
+                break
+            fi
         fi
+    done
+    
+    if [ "$found_pkg" = false ]; then
+        echo "No such package"
     fi
+
     export CDPATH="$cd_path"
 }
 complete -F _roscd roscd
